550438d06db2e648e84b8a7980aac8618b4de834
Frederico Nakajima <fredericonakajimanakajima@gmail.com>
2026-08-13 17:09:45 -0300

    feat: adiciona persistencia SQLite com Knex

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import fastify from 'fastify'
-
-const app = fastify()
-
-export default app
-
-app.get('/hello', async () => {
-  return 'Hello'
-})
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import app from './app.js'
+import fastify from 'fastify'
+import { knex } from './database'
 
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('Server is running on port 3333')
-  })
+const app = fastify()
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server is running on port 3333')
+})
